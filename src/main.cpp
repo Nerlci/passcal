5921a5d58b4765d0c9af5fb584d21c6866f14f1a
Nerlci <nerlci.underline@outlook.com>
2024-09-04 21:51:25 +0800

    Add support for compiling to object files

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/MC/TargetRegistry.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Target/TargetMachine.h>
+#include <llvm/TargetParser/Host.h>
+
 #include "CodeGenVisitor.h"
 #include "PascalSLexer.h"
 #include "PascalSParser.h"
@@ -24,6 +31,49 @@ int main(int argc, const char* argv[]) {
     codeGen.visit(tree);
 
     codeGen.module->print(outs(), nullptr);
+
+    InitializeAllTargetInfos();
+    InitializeAllTargets();
+    InitializeAllTargetMCs();
+    InitializeAllAsmParsers();
+    InitializeAllAsmPrinters();
+
+    // 获取目标机器
+    std::string targetTriple = llvm::sys::getDefaultTargetTriple();
+    std::cout << "Target triple: " << targetTriple << "\n";
+    codeGen.module->setTargetTriple(targetTriple);
+
+    std::string error;
+
+    const llvm::Target* target = llvm::TargetRegistry::lookupTarget(targetTriple, error);
+    if (!target) {
+        llvm::errs() << "Unable to find target\n";
+        return 1;
+    }
+
+    llvm::TargetOptions options;
+    std::unique_ptr<llvm::TargetMachine> targetMachine(target->createTargetMachine(targetTriple, "generic", "", options, llvm::Reloc::PIC_));
+    codeGen.module->setDataLayout(targetMachine->createDataLayout());
+
+    // 设置输出文件名
+    std::error_code EC;
+    llvm::raw_fd_ostream dest("output.o", EC, llvm::sys::fs::OF_None);
+    if (EC) {
+        llvm::errs() << "Could not open file: " << EC.message();
+        return 1;
+    }
+
+    llvm::legacy::PassManager pass;
+    if (targetMachine->addPassesToEmitFile(pass, dest, nullptr, llvm::CodeGenFileType::ObjectFile)) {
+        llvm::errs() << "TargetMachine can't emit a file of this type\n";
+        return 1;
+    }
+
+    pass.run(*codeGen.module);
+    dest.flush();
+
+    // 清理
+    llvm::llvm_shutdown();
 
     return 0;
 }
