970191ec085dee6be67c8e9a36f1a3391e2b795e
Jingyang Kang <kjy20030228@icloud.com>
2024-09-05 02:25:56 +0800

    Implement sub program IR generation, except for the return value of functions. (#18)
    * temp
    
    * Add CodeGenVisitor::visitSubprogramDeclaration, TODO:
    Handle return value of function, needs to collab with callProcedureStatement.
    
    * Remove redundant code and comment.

--- src/CodeGenVisitor.cpp
+++ src/CodeGenVisitor.cpp
@@ -1,5 +1,6 @@
 #include "CodeGenVisitor.h"
 #include "Exception/SemanticException.h"
+#include "PascalSParser.h"
 
 CodeGenVisitor::CodeGenVisitor()
     : builder(context) {
@@ -15,28 +16,20 @@ antlrcpp::Any CodeGenVisitor::visitProgramHead(PascalSParser::ProgramHeadContext
     auto program_id_node = ctx->ID();
     std::string program_name = program_id_node->getText();
     module = std::make_unique<Module>(program_name, context);
-
-    llvm::FunctionType* mainFuncType = llvm::FunctionType::get(Type::getInt32Ty(context), false);
-    llvm::Function* mainFunc = llvm::Function::Create(mainFuncType, llvm::Function::ExternalLinkage, "main", module.get());
-    llvm::BasicBlock* mainEntry = llvm::BasicBlock::Create(context, "mainEntry", mainFunc);
-
-    builder.SetInsertPoint(mainEntry);
-
+    llvm::FunctionType* main_func_type = llvm::FunctionType::get(Type::getInt32Ty(context), false);
+    llvm::Function* main_func = llvm::Function::Create(main_func_type, llvm::Function::ExternalLinkage, "main", module.get());
+    llvm::BasicBlock* main_entry = llvm::BasicBlock::Create(context, "mainEntry", main_func);
+    builder.SetInsertPoint(main_entry);
     return visitChildren(ctx);
 }
 
 antlrcpp::Any CodeGenVisitor::visitProgramBody(PascalSParser::ProgramBodyContext* ctx) {
-    visitChildren(ctx);
-
-    // Return 0 at the end of the program
-    // TODO: Return different values for procedures and functions
-    builder.CreateRet(ConstantInt::get(Type::getInt32Ty(context), 0));
-
-    return nullptr;
+    auto res = visitChildren(ctx);
+    builder.CreateRet(ConstantInt::get(current_return_type, 0));
+    return res;
 }
 
 antlrcpp::Any CodeGenVisitor::visitConstDeclaration(PascalSParser::ConstDeclarationContext* ctx) {
-    // Generate LLVM IR for constant declaration
     if (ctx->constDeclaration() != nullptr) {
         visit(ctx->constDeclaration());
     }
@@ -56,7 +49,6 @@ antlrcpp::Any CodeGenVisitor::visitConstDeclaration(PascalSParser::ConstDeclarat
 
 // @return llvm::Value*
 antlrcpp::Any CodeGenVisitor::visitConstVariable(PascalSParser::ConstVariableContext* ctx) {
-    // Generate LLVM IR for constant variable
     llvm::Value* value = nullptr;
 
     if (ctx->ID() != nullptr) {
@@ -93,7 +85,6 @@ antlrcpp::Any CodeGenVisitor::visitConstVariable(PascalSParser::ConstVariableCon
 }
 
 antlrcpp::Any CodeGenVisitor::visitTypeDeclaration(PascalSParser::TypeDeclarationContext* ctx) {
-    // Generate LLVM IR for type declaration
     if (ctx->typeDeclaration() != nullptr) {
         visit(ctx->typeDeclaration());
     }
@@ -109,7 +100,6 @@ antlrcpp::Any CodeGenVisitor::visitTypeDeclaration(PascalSParser::TypeDeclaratio
 }
 
 antlrcpp::Any CodeGenVisitor::visitVarDeclarations(PascalSParser::VarDeclarationsContext* ctx) {
-    // Generate LLVM IR for variable declarations
     if (ctx->varDeclaration() != nullptr) {
         auto var_declarations = std::any_cast<std::map<std::string, llvm::Type*>>(visit(ctx->varDeclaration()));
 
@@ -127,7 +117,6 @@ antlrcpp::Any CodeGenVisitor::visitVarDeclarations(PascalSParser::VarDeclaration
 
 // @return std::map<std::string, llvm::Type*>
 antlrcpp::Any CodeGenVisitor::visitVarDeclaration(PascalSParser::VarDeclarationContext* ctx) {
-    // Generate LLVM IR for variable declaration
     std::map<std::string, llvm::Type*> var_declarations;
     if (ctx->varDeclaration() != nullptr) {
         auto prev_var_declarations = std::any_cast<std::map<std::string, llvm::Type*>>(visit(ctx->varDeclaration()));
@@ -151,7 +140,6 @@ antlrcpp::Any CodeGenVisitor::visitVarDeclaration(PascalSParser::VarDeclarationC
 
 // @return std::vector<std::string>
 antlrcpp::Any CodeGenVisitor::visitIdentifierList(PascalSParser::IdentifierListContext* ctx) {
-    // Generate LLVM IR for identifier list
     std::vector<std::string> identifiers;
     std::string identifier = ctx->ID()->getText();
     identifiers.push_back(identifier);
@@ -166,7 +154,6 @@ antlrcpp::Any CodeGenVisitor::visitIdentifierList(PascalSParser::IdentifierListC
 
 // @return llvm::Type*
 antlrcpp::Any CodeGenVisitor::visitType(PascalSParser::TypeContext* ctx) {
-    // Generate LLVM IR for type
     llvm::Type* type = nullptr;
     if (ctx->standardType() != nullptr) {
         type = std::any_cast<llvm::Type*>(visit(ctx->standardType()));
@@ -204,7 +191,6 @@ antlrcpp::Any CodeGenVisitor::visitType(PascalSParser::TypeContext* ctx) {
 
 // @return std::vector<std::pair<int, int>>
 antlrcpp::Any CodeGenVisitor::visitPeriods(PascalSParser::PeriodsContext* ctx) {
-    // Generate LLVM IR for periods
     std::vector<std::pair<int, int>> periods;
     if (ctx->periods() != nullptr) {
         auto next_periods = std::any_cast<std::vector<std::pair<int, int>>>(visit(ctx->periods()));
@@ -219,7 +205,6 @@ antlrcpp::Any CodeGenVisitor::visitPeriods(PascalSParser::PeriodsContext* ctx) {
 
 // @return std::pair<int, int>
 antlrcpp::Any CodeGenVisitor::visitPeriod(PascalSParser::PeriodContext* ctx) {
-    // Generate LLVM IR for period
     auto start_const = (llvm::Constant*)std::any_cast<llvm::Value*>(visit(ctx->constVariable(0)));
     auto end_const = (llvm::Constant*)std::any_cast<llvm::Value*>(visit(ctx->constVariable(1)));
     int start = start_const->getUniqueInteger().getLimitedValue();
@@ -230,7 +215,6 @@ antlrcpp::Any CodeGenVisitor::visitPeriod(PascalSParser::PeriodContext* ctx) {
 
 // @return llvm::Type*
 antlrcpp::Any CodeGenVisitor::visitStandardType(PascalSParser::StandardTypeContext* ctx) {
-    // Generate LLVM IR for standard type
     llvm::Type* type = nullptr;
     if (ctx->INTEGER() != nullptr) {
         type = Type::getInt32Ty(context);
@@ -243,19 +227,80 @@ antlrcpp::Any CodeGenVisitor::visitStandardType(PascalSParser::StandardTypeConte
     } else {
         type = Type::getVoidTy(context);
     }
-
     return type;
 }
 
 antlrcpp::Any CodeGenVisitor::visitExpression(PascalSParser::ExpressionContext* ctx) {
-    // Generate LLVM IR for expression
     return visitChildren(ctx);
 }
 
-// Implement other visit methods as needed
+// @return nullptr
+antlrcpp::Any CodeGenVisitor::visitSubprogramDeclaration(PascalSParser::SubprogramDeclarationContext* ctx) {
+    auto sub_program_id_node = ctx->subprogramHead()->ID();
+    std::string sub_program_name = sub_program_id_node->getText();
+    std::vector<llvm::Type*> param_types;
+    llvm::Type* return_type = llvm::Type::getVoidTy(context);
+    if (ctx->subprogramHead()->formalParameter()->parameterLists()) {
+        std::vector<PascalSParser::ParameterListContext*> param_lists_vec;
+        auto ptr = ctx->subprogramHead()->formalParameter()->parameterLists();
+        while (ptr->parameterLists() != nullptr) {
+            param_lists_vec.push_back(ptr->parameterList());
+            ptr = ptr->parameterLists();
+        }
+        param_lists_vec.push_back(ptr->parameterList());
+        // Iterate over each parameter list
+        for (auto& param_list : param_lists_vec) {
+            // Each paramList might have multiple parameters with the same type, e.g., `a, b: integer`
+            PascalSParser::ValueParameterContext* value_parameter = nullptr;
+            if (param_list->varParameter()) {
+                value_parameter = param_list->varParameter()->valueParameter();
+            } else if (param_list->valueParameter()) {
+                value_parameter = param_list->valueParameter();
+            } else {
+                throw SemanticException(filename, ctx->getStart()->getLine(), ctx->getStart()->getCharPositionInLine(),
+                    "Parameter list must be either a value or var parameter");
+            }
+            llvm::Type* param_list_type = std::any_cast<llvm::Type*>(visitStandardType(value_parameter->standardType()));
+            std::vector<antlr4::tree::TerminalNode*> ident_list_vec;
+            auto ptr = value_parameter->identifierList();
+            while (ptr->identifierList() != nullptr) {
+                ident_list_vec.push_back(ptr->ID());
+                ptr = ptr->identifierList();
+            }
+            ident_list_vec.push_back(ptr->ID());
+            for (auto ident : ident_list_vec) {
+                std::string ident_str = ident->getText();
+                param_types.push_back(param_list_type);
+            }
+        }
+        if (ctx->subprogramHead()->PROCEDURE() != nullptr) {
+        } else if (ctx->subprogramHead()->FUNCTION() != nullptr) {
+            return_type = std::any_cast<llvm::Type*>(visitStandardType(ctx->subprogramHead()->standardType()));
+        } else {
+            throw SemanticException(filename, ctx->getStart()->getLine(), ctx->getStart()->getCharPositionInLine(),
+                "Subprogram declaration must be either a function or a procedure");
+        }
+    }
+    param_types = std::vector<llvm::Type*>(param_types.rbegin(), param_types.rend()); // reverse
+    llvm::FunctionType* sub_program_type = llvm::FunctionType::get(return_type, param_types, false);
+    llvm::Function* sub_program = llvm::Function::Create(sub_program_type, llvm::Function::ExternalLinkage, sub_program_name, module.get());
+    llvm::BasicBlock* sub_program_entry = llvm::BasicBlock::Create(context, ctx->subprogramHead()->ID()->toString(), sub_program);
+    Scope* sub_program_scope = new Scope(scope);
+    auto prev_insert_point = builder.saveIP();
+    auto prev_return_type = current_return_type;
+    auto prev_scope = scope;
+    scope = sub_program_scope;
+    current_return_type = return_type;
+    builder.SetInsertPoint(sub_program_entry);
+    auto res = visitChildren(ctx);
+    scope = prev_scope;
+    current_return_type = prev_return_type;
+    builder.restoreIP(prev_insert_point);
+    // TODO: handle return value of function, needs to collab with callProcedureStatement
+    return res;
+}
 
 Value* CodeGenVisitor::getArrayElement(Value* array, std::vector<Value*> index) {
-    // Generate LLVM IR for getting array element
     Type* array_type = ((AllocaInst*)array)->getAllocatedType();
     std::vector<std::pair<int, int>> array_info = scope->getArray(array_type);
     // if (array_info.size() != index.size()) {
@@ -273,7 +318,6 @@ Value* CodeGenVisitor::getArrayElement(Value* array, std::vector<Value*> index) 
 }
 
 Value* CodeGenVisitor::getRecordElement(Value* record, std::string& field) {
-    // Generate LLVM IR for getting record element
     Type* record_type = ((AllocaInst*)record)->getAllocatedType();
     std::map<std::string, int> record_info = scope->getRecord(record_type);
     int index = record_info[field];
