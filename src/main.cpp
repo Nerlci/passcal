22c5a9cce61be795a52fbe26358b8be24ed12b21
Ruining Chen <nerlci.underline@outlook.com>
2024-09-04 23:15:46 +0800

    Implement and refine command line interface (#12) (#13)
    * Add support for compiling to object files
    
    * Use getopt to parse command line options

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,56 @@
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/MC/TargetRegistry.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Target/TargetMachine.h>
+#include <llvm/TargetParser/Host.h>
+
+#include <getopt.h>
+
 #include "CodeGenVisitor.h"
 #include "PascalSLexer.h"
 #include "PascalSParser.h"
 
 using namespace antlr4;
 
-int main(int argc, const char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+int main(int argc, char* argv[]) {
+    int opt;
+    std::string output_file = "a.out";
+
+    struct option long_options[] = {
+        { "help", optional_argument, nullptr, 'h' },
+        { "output", optional_argument, nullptr, 'o' },
+    };
+
+    while ((opt = getopt_long(argc, argv, "ho:", long_options, nullptr)) != -1) {
+        switch (opt) {
+        case 'h':
+            // 当用户输入 --help 时打印帮助信息
+            printf("Usage: %s [options] input_file\n", argv[0]);
+            printf("Options:\n");
+            printf("  -h, --help            Display this information.\n");
+            printf("  -o, --output <file>   Place the output into <file>.>\n");
+            return 0;
+        case 'o':
+            // 保存输出文件名
+            output_file = optarg;
+            break;
+        case '?':
+            // 当遇到未知的选项时
+            fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+            return 1;
+        default:
+            abort();
+        }
+    }
+
+    // 检查是否有输入文件
+    if (optind >= argc) {
+        fprintf(stderr, "No input file specified.\n");
         return 1;
     }
 
-    std::string filename = argv[1];
+    std::string filename = argv[optind];
     std::ifstream stream;
     stream.open(filename);
     ANTLRInputStream input(stream);
@@ -24,6 +64,48 @@ int main(int argc, const char* argv[]) {
     codeGen.visit(tree);
 
     codeGen.module->print(outs(), nullptr);
+
+    InitializeAllTargetInfos();
+    InitializeAllTargets();
+    InitializeAllTargetMCs();
+    InitializeAllAsmParsers();
+    InitializeAllAsmPrinters();
+
+    // 获取目标机器
+    std::string targetTriple = llvm::sys::getDefaultTargetTriple();
+    codeGen.module->setTargetTriple(targetTriple);
+
+    std::string error;
+
+    const llvm::Target* target = llvm::TargetRegistry::lookupTarget(targetTriple, error);
+    if (!target) {
+        llvm::errs() << "Unable to find target\n";
+        return 1;
+    }
+
+    llvm::TargetOptions options;
+    std::unique_ptr<llvm::TargetMachine> targetMachine(target->createTargetMachine(targetTriple, "generic", "", options, llvm::Reloc::PIC_));
+    codeGen.module->setDataLayout(targetMachine->createDataLayout());
+
+    // 设置输出文件名
+    std::error_code EC;
+    llvm::raw_fd_ostream dest(output_file, EC, llvm::sys::fs::OF_None);
+    if (EC) {
+        llvm::errs() << "Could not open file: " << EC.message();
+        return 1;
+    }
+
+    llvm::legacy::PassManager pass;
+    if (targetMachine->addPassesToEmitFile(pass, dest, nullptr, llvm::CodeGenFileType::ObjectFile)) {
+        llvm::errs() << "TargetMachine can't emit a file of this type\n";
+        return 1;
+    }
+
+    pass.run(*codeGen.module);
+    dest.flush();
+
+    // 清理
+    llvm::llvm_shutdown();
 
     return 0;
 }
